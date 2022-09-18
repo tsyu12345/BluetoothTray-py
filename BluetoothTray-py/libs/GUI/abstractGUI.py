55ae0b7148fae1a405c9148dbec047027187586f
syu_asus_Note <syukunt@icloud.com>
2022-09-19 00:15:26 +0900

    icon表示と、起動用メソッドを追加。

--- BluetoothTray-py/libs/GUI/abstractGUI.py
+++ BluetoothTray-py/libs/GUI/abstractGUI.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Final as const 
+from typing import Final as const , Callable
 from abc import ABC, abstractmethod
 
 class AbsUI(ABC):
@@ -11,43 +11,36 @@ class AbsUI(ABC):
         Args:\n
             scran_devices (list[str]): 検出済みのbluetoothデバイス名\n #NOTE:将来的に型は変更される可能性あり
         """
-    
-    
     @abstractmethod
-    def show_devices(self) -> None:
+    def addEventListener(self, event: str, callback: Callable) -> None:
         """_summary_\n
-        検出したデバイスの一覧を表示する\n
+        イベントリスナーの追加\n
+        Args:\n
+            event (str): イベント名\n
+            callback (callable): コールバック関数\n
         """
         pass
+    """
+    @abstractmethod
+    def show_devices(self) -> None:
+        
     
     @abstractmethod
     def connect(self) -> None:
-        """_summary_\n
-        指定デバイスへBluetooth接続を試みる\n
-        """
-        pass
+    
     
     @abstractmethod
     def disconnect(self) -> None:
-        """_summary_\n
-        接続中のデバイスとのBluetooth接続を切断する\n
-        """
-        pass
+        
     
     
     @abstractmethod
     def save_devices(self) -> None:
-        """_summary_\n
-        過去にデバイス情報を保存する\n
-        """
-        pass
+
     
     @abstractmethod
     def load_devices(self) -> None:
-        """_summary_\n
-        過去に保存したデバイス情報を読み込む\n
-        """
-        pass
     
+    """
     
     
